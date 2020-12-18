3eef811493593f4fea14a6d5ade7deb900ea2cbd
Quynh Nguyen Van <qnv164@gmail.com>
2020-12-18 09:09:52 +0700

    404 Page not Found

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,7 +9,7 @@
                 <div class="col-xl-5 col-lg-6 col-md-7">
                     <div class="text-center">
                         <h1 class="display-1">4<img src="{{ url('assets/img/404.png') }}" alt="&#x1f635;">4</h1>
-                        <p>The page you were looking for was not found. Redirecting to Home after <span id="countdown">10</span> seconds</p>
+                        <p>The page you were looking for was not found. (<span id="countdown">10</span>s)</p>
                         <div class="mt-2">
                             <a class="fs-0" href="/home"><span class="pallette-grey-4">&larr; Back to Home</span></a>
                         </div>
@@ -30,7 +30,7 @@
         seconds = seconds - 1;
         if (seconds < 0) {
             // Chnage your redirection link here
-            window.location = "http://cse.capstone.vn/home";
+            window.location = "/home";
         } else {
             // Update remaining seconds
             document.getElementById("countdown").innerHTML = seconds;
